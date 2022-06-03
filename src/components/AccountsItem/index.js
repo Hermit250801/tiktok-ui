a1e8852490760df0957a0fe49022c5e652f27e0a
Hermit <maitruonghandsome@gmail.com>
2022-06-03 23:21:34 +0700

    Xây dựng UI phần Header #2

---
+++ src/components/AccountsItem/index.js
@@ -0,0 +1,27 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountsItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+function AccountsItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/ea0854578085ab26effc2c7b8cefa270~c5_100x100.jpeg?x-expires=1654441200&x-signature=ydzUYFmRIJMCFeYmKvEhl2yo9tA%3D"
+                alt=""
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyễn Văn A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('userName')}>nguyenvanaa</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountsItem;
